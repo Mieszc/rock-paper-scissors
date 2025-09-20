738e3b7dfe26bc390ad700097a9fbd2245d69541
Mieszc <168421615+Mieszc@users.noreply.github.com>
2025-09-20 14:17:07 +0200

    FUNCTION GENERATES COMPUTER'S CHOICE
    Math.Random as randomizer of choice

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('test');
+let computers_choice; 
+
+function generateComuptersChoice() {
+    let num = Math.floor(Math.random()*10); 
+    if(num <= 3) {
+        computers_choice = "Rock";
+    }
+    else if(num > 3 && num <= 6) {
+        computers_choice = "Paper";
+    }
+    else {
+        computers_choice = "Scissors";
+    }
+}
+
