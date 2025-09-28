78aca8e65675cdf984361c2c5bdfc89c0a66edad
Mieszc <168421615+Mieszc@users.noreply.github.com>
2025-09-28 18:22:54 +0200

    add variable to store choices message
    add function to display players choices

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let computers_choice; 
 let users_choice;
+let players_choices;
 
 function generateComuptersChoice() {
     let num = Math.floor(Math.random()*10);
@@ -17,5 +18,10 @@ function generateComuptersChoice() {
 function captureUsersChoice() {
     users_choice = prompt("Choose one: Rock, Paper, or Scissors.", "");
     toString(users_choice);
+}
+
+function showChoices() {
+    players_choices = `Computer's choice is ${computers_choice} and your choice is ${users_choice}.`;
+    console.log(players_choices);
 }
 
